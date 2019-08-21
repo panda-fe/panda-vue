1833de74b6eef05cfae989f292a29a3b8ab13aa5
xinwang <x020198@qq.com>
2019-08-21 16:09:39 +0800

    feat: 1. 增加自定义类名       2. 增加slot列，删除不必要的列，可以通过slot实现       3. 合并排序列和普通列       4. 分页默认右对齐

--- packages/basictable/src/main.vue
+++ packages/basictable/src/main.vue
@@ -5,108 +5,96 @@
 */
 
 <template>
-  <div class='basic-table'>
+  <div class='basic-table' :class="className">
     <!--列表-->
-    <el-table :data="dataList" border stripe @sort-change="sortChange" @selection-change="handleSelectionChange">
+    <el-table :data="dataList"
+              v-loading="listLoading"
+              border
+              stripe
+              @sort-change="sortChange"
+              @selection-change="handleSelectionChange">
+      <!--选择列-->
+      <el-table-column v-if="hasSelect"
+                       type="selection"
+                       width="55">
+      </el-table-column>
       <!--序号列-->
-      <el-table-column type="selection" width="55" v-if="hasSelect"></el-table-column>
-      <el-table-column type="index" width="40" v-if="hasIndex"></el-table-column>
+      <el-table-column v-if="hasIndex"
+                       type="index"
+                       width="40"
+                       label="序号">
+      </el-table-column>
+
       <template v-for="column in columns">
-        
+
         <!--按钮操作列-->
         <el-table-column v-if="column.operations"
                          :prop="column.prop"
                          :label="column.label"
-                         :width="column.width||(column.operations.length*30+100)"
+                         :width="column.width||(column.operations.length*50+100)"
                          :formatter="column.formatter"
-                         fixed="right"
                          :min-width='column.minwidth'>
           <template slot-scope="scope">
-            <div v-for="(operate,index) in column.operations" style="display: inline-block" :class="(operate.show?operate.show(scope.$index, scope.row):true)?'show-sep':''">
+            <div v-for="(operate,index) in column.operations"
+                 style="display: inline-block"
+                 :class="(operate.show?operate.show(scope.$index, scope.row):true)?'show-sep':''">
               <el-tooltip placement="top"
                           :content="operate.label"
                           :disabled="!operate.tooltips"
                           :key="index">
-                <el-popover :placement="operate.popoverPlacement || 'top'" :width="operate.popoverWidth || 150" :ref="`popover-${index}-${scope.$index}`" v-if="operate.popover">
+                <el-popover v-if="operate.popover"
+                            :placement="operate.popoverPlacement || 'top'"
+                            :width="operate.popoverWidth || 150"
+                            :ref="`popover-${index}-${scope.$index}`" >
                   <p style="text-align: center">{{operate.tips(scope.$index, scope.row)}}</p>
                   <div style="text-align: center; margin: 0">
                     <el-button size="mini" @click="handleConfirm(index, scope.$index, scope.row, operate.func)">确认</el-button>
                     <el-button size="mini" @click="handleClose(index, scope.$index)">取消</el-button>
                   </div>
-                  <el-button slot="reference" :type="operate.type||'text'" class="form-icon"
+                  <el-button slot="reference"
+                             :type="operate.type||'text'"
+                             class="form-icon"
                              v-show="operate.show?operate.show(scope.$index, scope.row):true"
                              :key="index"
                              :icon="operate.icon"
-                             :style="operate.disabledfunc?!operate.disabledfunc(scope.$index,scope.row)?'color:'+(operate.color||'#5576ed'):'':'color:'+(operate.color||'#5576ed')"
-                             :disabled="operate.disabledfunc ?operate.disabledfunc(scope.$index,scope.row) :false">{{(operate.type&&operate.type!=='text')||!operate.icon?operate.label:''}}
+                             :style="handleButtonStyle(operate, scope)"
+                             :disabled="operate.disabledfunc ?operate.disabledfunc(scope.$index,scope.row) :false">
+                    {{(operate.type&&operate.type!=='text')||!operate.icon?operate.label:''}}
                   </el-button>
                 </el-popover>
-                <el-button :type="operate.type||'text'" class="form-icon"
+                <el-button :type="operate.type||'text'"
+                           class="form-icon"
                            v-else
                            v-show="operate.show?operate.show(scope.$index, scope.row):true"
                            :key="index"
                            :icon="operate.icon"
-                           :style="operate.disabledfunc?!operate.disabledfunc(scope.$index,scope.row)?'color:'+(operate.color||'#5576ed'):'':'color:'+(operate.color||'#5576ed')"
+                           :style="handleButtonStyle(operate, scope)"
                            @click="operate.func(scope.$index, scope.row)"
-                           :disabled="operate.disabledfunc ?operate.disabledfunc(scope.$index,scope.row) :false">{{(operate.type&&operate.type!=='text')||!operate.icon?operate.label:''}}
+                           :disabled="operate.disabledfunc ?operate.disabledfunc(scope.$index,scope.row) :false">
+                  {{(operate.type&&operate.type!=='text')||!operate.icon?operate.label:''}}
                 </el-button>
               </el-tooltip>
             </div>
-          
+
           </template>
         </el-table-column>
-        
-        <!--点击内容弹出的列-->
-        <el-table-column v-else-if="column.func || column.href"
+
+        <el-table-column v-else-if="column.slotName"
                          :prop="column.prop"
                          :label="column.label"
                          :width="column.width"
                          :formatter="column.formatter"
                          :show-overflow-tooltip="column.tooltip"
                          :min-width='column.minwidth'>
-          <template slot-scope="scope">
-            <!--<el-tooltip placement="top" :content="scope.row[column.prop]">     @mouseenter="enter"
-                 @mouseleave="leave"-->
-            <span v-if='column.disabled && column.disabled(scope.$index, scope.row)'>{{scope.row[column.prop]}}</span>
-            <a @click="column.func(scope.$index, scope.row)"
-               class='clickable-link'
-               v-else-if='scope.row[column.prop]'>
-              {{scope.row[column.prop]}}
-            </a>
-            <span v-else-if='column.formatBeside'>{{column.formatBeside(scope.$index, scope.row)}}</span>
-            <i v-if="column.icon&&column.showIcon&&column.showIcon(scope.$index, scope.row)" :class="column.icon"></i>
-            <!--</el-tooltip>-->
+          <template v-slot="scope">
+            <slot :name="'col-' + column.slotName"
+                  :row="scope.row"/>
           </template>
         </el-table-column>
-        
-        <!--角色管理使用，无需使用可以删除-->
-        <el-table-column v-else-if="column.list"
-                         :prop="column.prop"
-                         :label="column.label"
-                         :width="column.width"
-                         :formatter="column.formatter"
-                         :show-overflow-tooltip="column.tooltip"
-                         :min-width='column.minwidth'>
-          <template slot-scope="scope">
-            <el-tag v-for='item in scope.row[column.prop]' :key='item.id'>
-              {{item.roleName}}
-            </el-tag>
-          </template>
-        </el-table-column>
-        
-        <!--排序列，无需使用可以删除-->
-        <el-table-column v-else-if="column.sortable"
-                         sortable="custom"
-                         :prop="column.prop"
-                         :label="column.label"
-                         :width="column.width"
-                         :formatter="column.formatter"
-                         :show-overflow-tooltip="column.tooltip"
-                         :min-width='column.minwidth'>
-        </el-table-column>
-        
+
         <!--普通展示列-->
         <el-table-column v-else
+                         :sortable="column.sortable?'custom':false"
                          :prop="column.prop"
                          :label="column.label"
                          :width="column.width"
@@ -116,9 +104,9 @@
         </el-table-column>
       </template>
     </el-table>
-    
+
     <!--分页  -->
-    <el-row>
+    <el-row type="flex" justify="end">
       <el-col :span="24">
         <el-pagination layout="total, sizes, prev, pager, next"
                        @size-change="handleSizeChange"
@@ -127,23 +115,32 @@
                        :page-size="rows"
                        :page-sizes="[10, 20, 30, 50, 80, 100]"
                        :total="total"
-                       class="table-pag"></el-pagination>
+                       class="table-pag">
+        </el-pagination>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-  const sortMap = { descending: 'desc', ascending: 'ASC' }
+  const sortMap = { descending: 2, ascending: 1 }
   export default {
     name: 'BasicTable',
     props: {
-      api: { type: Function },    // 下个项目换名字
+      // 获取数据的接口
+      api: { type: Function },
+      // 自定义类名
+      className: {type: String},
+      // 表格信息
       columns: { type: Array },
+      // 初始化条件
       initCondition: { type: Object },
-      initSearch: { type: Boolean, default: true },  // 是否初始化的时候就query
-      hasIndex: { type: Boolean, default: false },   // 是否需要序号列
-      hasSelect: { type: Boolean, default: false }   // 是否需要选择框
+      // 是否初始化的时候就query
+      initSearch: { type: Boolean, default: true },
+      // 是否需要序号列
+      hasIndex: { type: Boolean, default: false },
+      // 是否需要选择框
+      hasSelect: { type: Boolean, default: false }
     },
     data() {
       return {
@@ -157,8 +154,7 @@
           orderColumn: '',
           orderBy: ''
         },
-        multipleSelection: [],
-        popoverShow: false
+        multipleSelection: []
       }
     },
     methods: {
@@ -166,8 +162,8 @@
        * 获得表格内容
        */
       getlist(param) {
+        this.listLoading = true
         this.api(param).then(res => {
-          this.listLoading = false
           const { total, dataList } = res.data
           this.dataList = dataList
           this.total = total
@@ -177,19 +173,18 @@
           this.listLoading = false
         })
       },
-      
+
       /**
        * 不含查询条件的查询
        */
       reSearch() {
-        this.listLoading = true
         this.page = 1
         const param = Object.assign({}, this.storeParams, this.sortParams)
         param.rows = this.rows
         param.page = this.page
         this.getlist(param)
       },
-      
+
       /**
        * 查询按钮查询
        */
@@ -197,24 +192,22 @@
         this.storeParams = Object.assign({}, params)
         this.reSearch()
       },
-      
+
       /**
        * 处理页数变化
        */
       handleSizeChange(val) {
-        this.listLoading = true
         this.rows = val
         const param = Object.assign({}, this.storeParams, this.sortParams)
         param.rows = val
         param.page = this.page
         setTimeout(() => this.getlist(param), 100)
       },
-      
+
       /**
        * 处理当前页改变
        */
       handleCurrentChange(val) {
-        this.listLoading = true
         this.page = val
         const param = Object.assign({}, this.storeParams, this.sortParams)
         param.rows = this.rows
@@ -226,7 +219,7 @@
        */
       sortChange({ column, prop, order }) {
         this.sortParams.orderColumn = prop || ''
-        this.sortParams.orderBy = order ? sortMap[order] : ''
+        this.sortParams.order = order ? sortMap[order] : ''
         this.reSearch()
       },
       /**
@@ -240,7 +233,7 @@
        */
       handleClose(index, rowIndex) {
         const name = `popover-${ index }-${ rowIndex }`
-        this.$refs[name][1].doClose()
+        this.$refs[name][0].doClose()
       },
       /**
        * popover确认
@@ -248,6 +241,13 @@
       handleConfirm(index, rowIndex, row, func) {
         func(rowIndex, row)
         this.handleClose(index, rowIndex)
+      },
+      /**
+       * 处理按钮样式
+       */
+      handleButtonStyle(operate, scope) {
+        const defalutColor = (operate.type && operate.type !== 'text') ? '#ffffff' : '#409EFF'
+        return operate.disabledfunc && operate.disabledfunc(scope.$index,scope.row) ? {} : {color: operate.color || defalutColor}
       }
     },
     mounted() {
@@ -257,21 +257,11 @@
 </script>
 
 <style lang="scss" scoped>
-  .basic-table {
-    .clickable-link {
-      font-size: 14px;
-      color: #07f;
-      cursor: pointer;
-    }
-    
-    .el-tag {
-      margin: 3px 10px;
-    }
-    
-    /deep/ .form-icon {
-      padding-top: 0;
-      padding-bottom: 0;
-      border-radius: 0;
-    }
+  .el-pagination{
+    display: flex;
+    justify-content: flex-end;
+  }
+  .show-sep+.show-sep{
+    margin-left: 10px;
   }
 </style>
